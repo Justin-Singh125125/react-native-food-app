6fbb4a676327c79814f634ae92752338d314918a
Justin Singh <justin.singh125125@gmail.com>
2020-02-24 21:43:43 -0800

    searching is working

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,16 +1,37 @@
 import React, { useState } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 
+//api's
+import yelp from "../utils/API/yelp";
+
 //components
 import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
 
 	const [searchTerm, setSearchTerm] = useState("");
+	const [results, setResults] = useState([]);
+	const [errorMessage, setErrorMessage] = useState("");
 
-	const handleSubmit = () => {
-		console.log(searchTerm)
+	const handleSubmit = async () => {
+		try {
+			const response = await yelp.get("/search", {
+				params: {
+					limit: 50,
+					term: searchTerm,
+					location: "san jose"
+				}
+			})
+
+			setResults(response.data);
+		}
+		catch (e) {
+			console.log(e);
+			setErrorMessage("Something went wrong! Please try again later")
+		}
+
 	}
+
 
 	return (
 		<View>
@@ -19,6 +40,8 @@ const SearchScreen = () => {
 				searchTerm={searchTerm}
 				setSearchTerm={setSearchTerm}
 			/>
+
+			{errorMessage ? <Text>{errorMessage}</Text> : null}
 		</View>
 	);
 };
---
+++ src/utils/API/yelp.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+//axios configuration
+export default axios.create({
+    baseURL: "https://api.yelp.com/v3/businesses",
+    headers: {
+        Authorization: 'Bearer Nm1jbiB-bqfD4x62BG_cx9fvD-eVwKh2MTrJboxNjKjti31KKJL2WPffnPCTh_9i62LlEXDa-jQQSoRxxkNJDgz5NUqRzhllqFMX_9xVwSUc7kBFW_zqJmtnPgRSXnYx'
+    }
+});
+
+
+
